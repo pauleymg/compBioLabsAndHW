dacebb67aa2276a7abca1a06b4ffba5d0ac8287d
pauleymg <pauley.marshall@tutanota.com>
2020-02-04 15:56:44 -0700

    Made README verbose, commented Lab01 shell-script & removed a redundant line

--- Labs/Lab01/Lab01finalproblem.sh
+++ Labs/Lab01/Lab01finalproblem.sh
@@ -1,3 +1,6 @@
-touch newData.csv
+# this script creates an abridged version of the predator-prey dataset.
+
+# remove unwanted columns and then pushes only the first row into a new spreadsheet file.
 cut -f 2-4 -d , PredPreyData.csv | head -n 1 > newData.csv
-cut -f 2-4 -d , PredPreyData.csv | tail -n 10 >> newData.csv
+# remove unwanted columns and append last ten rows to the file created in the previous step.
+cut -f 2-4 -d , PredPreyData.csv | tail >> newData.csv
--- README.txt
+++ README.txt
@@ -1,1 +1,7 @@
-This repository contains Labs and Homework for Sam Flaxman's EBIO 4420 class.
+This repository contains Labs and Homework for EBIO4420 Computational Biology at University of Colorado Boulder, taught by Samuel Flaxman.
+
+https://github.com/flaxmans/CompBio_on_git
+
+Location: CU Boulder main campus, Ketchum 1B20 (Ramaley N1B75 for lab on Friday)
+Meeting_Times: 11:00-11:50 AM Monday & Wednesday, 12:00-1:50 PM Friday
+Semester: Spring 2020
