6acb039b5bb9946d3ac3842c168a736df5df8083
pauleymg <pauley.marshall@tutanota.com>
2020-02-09 17:55:19 -0700

    Lab04 R script working to step 8d

--- Labs/Lab04/PAULEY_Lab04.R
+++ Labs/Lab04/PAULEY_Lab04.R
@@ -13,7 +13,7 @@ howMany <- 1:10 # how many times the for loop cycles
 # step 1 solution
 for(i in howMany){
     print(greeting)
-}
+} # prints message desired number of times
 
 #---- step 2 ----
 TimMoneyInitial <- 10 # Dollars
@@ -30,14 +30,14 @@ for(i in Weeks){
 }
 
 #---- step 3 ----
-currentPopulation <- 2000
+currentPopulation <- 2000 # number of individuals in the population, but not for long
 shrinkage <- 0.95 # 5% decline
-Years <- 1:7
+Years <- 1:7 # how many years to project forward
 
 # step 3 solution
 for (i in Years){
-    currentPopulation <- currentPopulation * shrinkage
-    print(currentPopulation)
+    currentPopulation <- currentPopulation * shrinkage # mutliply population to estimate decline
+    print(currentPopulation) # print result every loop cycle
 }
 
 #---- step 4 ----
@@ -57,19 +57,20 @@ for (i in 2:endTime){
 # PopulationSteps now contains population abundance data for each time step (for part 7)
 
 #---- step 5a ----
-QuantityOfZeroes <- 18
-RepeatedZero <- rep(0, QuantityOfZeroes) 
+QuantityOfZeroes <- 18 # number of positions desired in the vector
+RepeatedZero <- rep(0, QuantityOfZeroes) # spawn vector of desired length, filled with "0" at every position
 
 #---- step 5b ----
-multiplicationFactor <- 3
+multiplicationFactor <- 3 # it's an object with the number 3 in
 for (i in 1:QuantityOfZeroes){
-    RepeatedZero[i] <- i * multiplicationFactor
+    RepeatedZero[i] <- i * multiplicationFactor # multiply the position number by the object created above.
 }
 print(RepeatedZero)
 
 #---- step 5c ----
-Placeholder <- rep(0, QuantityOfZeroes)
-Placeholder[1] <- IntegerOne
+Placeholder <- rep(0, QuantityOfZeroes) # another 18 0s as a placeholder vector
+Placeholder[1] <- IntegerOne <- # place the number 1 at the first position
+print(Placeholder)
 
 #---- step 5d ----
 for (i in 2:QuantityOfZeroes){
@@ -79,31 +80,56 @@ print(Placeholder)
 
 #---- step 6 ----
 FibEnd <- 20 # how many fibonacci numbers we want to generate
-FibonacciSeq <- rep(0, FibEnd)
-FibonacciSeq[2] <- IntegerOne 
-for (i in 3:FibEnd){
-    FibonacciSeq[i] <- FibonacciSeq[i - IntegerOne] + FibonacciSeq[i - IntegerTwo]
+FibonacciSeq <- rep(0, FibEnd) # placeholder zeros, however many were specified above
+FibonacciSeq[2] <- IntegerOne # place 1 at the second position
+for (i in 3:FibEnd){ # we start at three to avoid overwriting the 0 & 1 in positions 1 & 2.
+    FibonacciSeq[i] <- FibonacciSeq[i - IntegerOne] + FibonacciSeq[i - IntegerTwo] #math of the Fibonacci sequence
 }
 print(FibonacciSeq)
 
 #---- step 7 ----
 # storage accomplished in step 4
 
-plot(TimeSteps, PopulationSteps, xlab = "Time Steps", ylab = "Population Abundance")
+plot(TimeSteps, PopulationSteps, xlab = "Time Steps", ylab = "Population Abundance") # Visualizing the data
 
 #---- step 8a ----
-Carbon <- read.csv("CO2_data_cut_paste.csv")
-str(Carbon)
+Carbon <- read.csv("CO2_data_cut_paste.csv") # load data
 
 #---- step 8b ----
 # read.csv with colClasses argument
-CarbonData <- read.csv(file = "CO2_data_cut_paste.csv", colClasses = c("integer", rep("numeric", 7)))
-str(CarbonData)
+CarbonData <- read.csv(file = "CO2_data_cut_paste.csv", colClasses = c("integer", rep("numeric", 7))) #load data with style
 
 # for loop
-totalColumns <- 8
-for (i in 2:totalColumns){
+for (i in 2:ncol(Carbon)){
     Carbon[,i] <- as.numeric(Carbon[,i])
     } # Matilde came up with this for-loop solution
-str(Carbon)
 
+#---- step 8c ----
+
+#-# for loop approach
+interim <- list() # creates a blank list to push into
+
+for (i in 2:nrow(Carbon)){
+    interim[[i]] <- ((Carbon[i,] - Carbon[i - 1,]) / Carbon[i - 1,]) * 100
+} # a non-nested for loop solution. Runs the math & pushes to an empty list
+
+NewCarbon <- do.call(what=rbind.data.frame, interim) # turn the list into a data frame (learned this trick from StackOverflow user Marek, who answered a similar question for someone else)
+
+NewCarbon$Year <- Carbon$Year[2:nrow(Carbon)] # The for-loop mangled the year column. This fixes it.
+
+
+#-# element by element approach
+func <- function(x) c(NA, (tail(x, - 1) - head(x, - 1)) / head(x, - 1) * 100) # function to calculate percent change
+
+NewCarbon2 <- sapply(Carbon[, sapply(Carbon, is.numeric)], func) # this, and the above function, based on MPhD's answer to isuckatcoding's question on StackOverflow.
+NewCarbon2 <- as.data.frame(NewCarbon2)
+NewCarbon2$Year <- Carbon$Year[1:nrow(Carbon)] # my function busted the year column. Fixed.
+NewCarbon2 <- NewCarbon2[-c(1),] # excising first row, which is all NA.
+
+# Compare results 
+na.exclude(NewCarbon) == na.exclude(NewCarbon2) # comparing the output of the for loop to that of the function.
+
+#---- step 8d ----
+
+#create a csv using the data from one of the two methods
+write.csv(NewCarbon, file = "YearlyPercentChangesInCO2.csv")
