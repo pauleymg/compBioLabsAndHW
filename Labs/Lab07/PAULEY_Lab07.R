5d2d062af38202c32c9aad1939c26db2224af9aa
pauleymg <pauley.marshall@tutanota.com>
2020-02-27 14:13:06 -0700

    Updated comments to be more descriptive

--- Labs/Lab07/PAULEY_Lab07.R
+++ Labs/Lab07/PAULEY_Lab07.R
@@ -5,21 +5,22 @@ setwd("Documents/Courses/EBIO4420/compBioLabsAndHW/Labs/Lab07/")
 
 #---- Problem 1 ----
 
-# create function, given base and height
+# create function to calculate area of a triangle, given base and height
 myTriangleArea <- function(base, height){
-    # step 1: multiply base * height * 0.5
+    # step 1: multiply
     myResult <- base * height * 0.5
     # step 2: return result
     return(myResult)
 }
 
 # demonstrate it works
+# suppose a triangle with base 10 units, and height of 9 units
 myTriangleArea(base = 10, height = 9)
 
 
 #---- Problem 2a ----
 
-# Build my own absolute value function, given scalar or vector numeric input
+# create an absolute value function, given scalar or vector numeric input
 myAbs <- function(input){
     # step 1: identify any negative values
     NegativeID <- input < 0
@@ -30,6 +31,7 @@ myAbs <- function(input){
 }
 
 # demonstrate it works
+# test a few scalar values (positive & negative)
 myAbs(input = 5)
 myAbs(input = -2.3)
 
@@ -39,30 +41,30 @@ myAbs(input = -2.3)
 # create test vector
 AbsTest <- c(1.1, 2, 0, -4.3, 9, -12)
 
-# demonstrate it works
+# demonstrate my absolute value function works with a vector input
 myAbs(AbsTest)
 
 
 #---- Problem 3 ----
 
-# create function, given quantity of desired fibonacci numbers
-suchFibonacci <- function(n){
-    if (n == 1){ # bonus 3a
-        FibStorage <- 0 # handle n = 1
-    } else if (n == 2){ # bonus 3a
-        FibStorage <- c(0, 1) # handle n = 2
-    } else if (n <= 0){ # bonus 3b
-        FibStorage <- "Warning: Fibonacci sequence cannot have no length or negative length" # return warning message if n is zero or negative
-    } else if (n%%1!=0){ # bonus 3b
-        FibStorage <- "Warning: Fibonacci sequence must have an integer length" # return warning message if n is not an integer
+# create function to generate the first "n" fibonacci numbers, given quantity
+suchFibonacci <- function(quantity){
+    if (quantity == 1){ # bonus 3a
+        FibStorage <- 0 # handle quantity = 1
+    } else if (quantity == 2){ # bonus 3a
+        FibStorage <- c(0, 1) # handle quantity = 2
+    } else if (quantity <= 0){ # bonus 3b
+        FibStorage <- "Warning: Fibonacci sequence cannot have no length or negative length" # return warning message if quantity is zero or negative
+    } else if (quantity%%1!=0){ # bonus 3b
+        FibStorage <- "Warning: Fibonacci sequence must have an integer length" # return warning message if quantity is not an integer
     } else { #main solution
         # preallocate storage vector of length equivalent to the given quantity
-        FibStorage <- rep(NA, n)
+        FibStorage <- rep(NA, quantity)
         # set first two fibonacci numbers
         FibStorage[1] <- 0
         FibStorage[2] <- 1
         # populate fibonacci numbers into storage vector
-        for (i in 3:n){
+        for (i in 3:quantity){
             FibStorage[i] <- FibStorage[i - 1] + FibStorage[i - 2]
         }
     }
@@ -70,6 +72,7 @@ suchFibonacci <- function(n){
     return(FibStorage)  
 }
 
+# demonstrate my fibonacci function works
 # testing: standard inputs
 suchFibonacci(3)
 suchFibonacci(42)
@@ -78,27 +81,29 @@ suchFibonacci(42)
 suchFibonacci(2)
 suchFibonacci(1)
 
-# testing: nonsensical inputs
+# testing: nonsensible inputs
 suchFibonacci(0)
 suchFibonacci(-6)
 suchFibonacci(7.2)
 
 #---- Problem 4a ----
 
-# create function, given two numbers 
+# create function to get the square of a difference, given two numbers 
 squareOfDifference <- function(x, y){
     # calculate the square of the difference
     sqDif <- (x - y) ^ 2
-    return(sqDif)
+    return(sqDif) #return result
 }
 
-# demonstation
+# demonstate it works
+# suppose two scalar values
 squareOfDifference(3, 5)
+# suppose a vector and a scalar value
 squareOfDifference(c(2, 4, 6), 4)
 
 #---- Problem 4b ----
 
-# create function, given a vector of numbers
+# create an arithmetic mean function, given a vector of numbers
 manualArithMean <- function(x){
     sumX <- sum(x) # sum elements
     newMean <- sumX / length(x) # divide sum by number of elements
@@ -110,18 +115,18 @@ manualArithMean(c(5, 10, 15))
 
 # load data
 labSevenData <- read.csv("../../../Sandbox/CompBio_on_git/Labs/Lab07/DataForLab07.csv")
-# test on data
+# test my arithmetic mean function on a data set
 manualArithMean(labSevenData$x)
 
 #---- Problem 4c ----
 
-# create function, given a vector of numbers
+# create function to calculate the sum of squares, given a vector of numbers
 SumOSquares <- function(input){
-    newMean <- manualArithMean(input) # get the mean of the input
+    newMean <- manualArithMean(input) # get the arithmetic mean of the input
     differenceVector <- squareOfDifference(input, newMean) # take difference between the mean and each data point, square it
     Result <- sum(differenceVector) # sum all elements
     return(Result)
 }
 
-# demonstration time
+# demonstrate my sum of squares function works on a data set
 SumOSquares(labSevenData$x)
