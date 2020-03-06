b4798fab4a1761e46a41ec123988b865cd5dbee9
pauleymg <pauley.marshall@tutanota.com>
2020-03-06 12:30:23 -0700

    Lab08: simplified Rscript, created documentation

--- Labs/Lab08/PAULEY_Lab08.R
+++ Labs/Lab08/PAULEY_Lab08.R
@@ -7,16 +7,14 @@ setwd("Documents/Courses/EBIO4420/compBioLabsAndHW/Labs/Lab08/")
 # This function is set up to model logistic population growth in discrete time increments. It will not generate the smooth curve of logistic growth over continuous time.
 LogiGrowthModel <- function(r, K, totalGenerations, initPopSize){
     
-    endTime <- totalGenerations # 3b
-    generations <- 1:endTime # This object holds all the time steps
+    generations <- 1:totalGenerations # Whole-number sequence of all time steps
     CarryingCap <- K # 3b
     IntrinsicGrowthRate <- r # 3b
-    abundance <- rep(0, endTime) # this object will hold population abundance at each time step
+    abundance <- rep(0, totalGenerations) # this object will hold population abundance at each time step
     abundance[1] <- initPopSize # 3b
     
-    for (i in 2:endTime){
-        interStep <- ( CarryingCap - abundance[i - 1] ) / CarryingCap # right-hand part of the equation, pushed into an transfer object
-        abundance[i] <- abundance[i - 1] + ( IntrinsicGrowthRate * abundance[i - 1] * interStep ) # left-hand part of the equation with transfer object, pushed into data-holding object
+    for (i in 2:totalGenerations){
+        abundance[i] <- abundance[i - 1] + ( IntrinsicGrowthRate * abundance[i - 1] * ( (CarryingCap - abundance[i - 1]) / CarryingCap ) )
     }
     
     plot(generations, abundance, xlab = "Time (in generations)", ylab = "Population Abundance", pch = 18, col = "dodgerblue", cex = 1.5, main = "Discrete-Time Logistic Growth Model") # 3c
